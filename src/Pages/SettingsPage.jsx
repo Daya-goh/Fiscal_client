ffac9e1f41c5aaab56efb069a84b8871253873c9
Justen Manni <mannijusten@gmail.com>
2022-09-20 10:01:19 +0800

    Commits for git push and pul sync

--- src/Pages/SettingsPage.jsx
+++ src/Pages/SettingsPage.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function SettingsPage() {
   return (
     <>
-      <h1>Settings Page</h1>
+      <div className="bg-white dark:bg-slate-900 py-6 sm:py-8 lg:py-12">
+        <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-8"></div>
+        </div>
+      </div>
     </>
   );
 }
